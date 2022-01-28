bedb9e2c433438c82ab2bb69e1e696683be0f2d6
Centauri07 <magnus.centauri07@gmail.com>
2022-01-28 17:19:29 +0800

    Added test

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,5 +13,12 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
 
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
     implementation("com.google.guava:guava:31.0.1-jre")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
 }
