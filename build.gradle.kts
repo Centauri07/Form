bcc9b781033c9d71290337372a6eba3ebd4189c6
Centauri07 <magnus.centauri07@gmail.com>
2022-01-28 19:47:10 +0800

    Added maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.31"
     java
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
@@ -14,12 +15,24 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
     implementation("com.google.guava:guava:31.0.1-jre")
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "me.centauri07.form"
+            artifactId = "Form"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
 }
