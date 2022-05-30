644d6260a0afac3bcdea96fdd7033d49431a096d
Centauri07 <magnus.centauri07@gmail.com>
2022-05-30 18:57:08 +0800

    Fixed error oops

--- src/main/kotlin/me/centauri07/form/FormModel.kt
+++ src/main/kotlin/me/centauri07/form/FormModel.kt
@@ -6,7 +6,7 @@ import me.centauri07.form.field.group.GroupFormField
 /**
  * @author Centauri07
  */
-abstract class FormModel(name: String, val submitOnFinish: Boolean): GroupFormField<FormField<*>>(name, true) {
+abstract class FormModel(name: String, description: String? = null, val submitOnFinish: Boolean): GroupFormField<FormField<*>>(name, description, true) {
 
     abstract fun setup(form: Form)
 
